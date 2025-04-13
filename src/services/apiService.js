f0efb5a24b07e05e2543cb4b514d5f806c081cb7
soli-vox <devisopen@gmail.com>
2025-04-13 14:27:23 +0545

    testing

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+console.log("API Base URL:", API_BASE_URL);
 
 axios.defaults.withCredentials = true;
 axios.defaults.withXSRFToken = true;
